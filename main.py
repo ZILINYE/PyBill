ac7e6855a439cd0a4d42aca0078639b3c1c48bc4
Tobyyyye <33171748+ZILINYE@users.noreply.github.com>
2023-03-13 15:55:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -49,19 +49,19 @@ class SplashPanel(QSplashScreen):
 
     def mousePressEvent(self, evt):
         pass
-        # 重写鼠标点击事件，阻止点击后消失
+
 
     def mouseDoubleClickEvent(self, *args, **kwargs):
         pass
-        # 重写鼠标移动事件，阻止出现卡顿现象
+
 
     def enterEvent(self, *args, **kwargs):
         pass
-        # 重写鼠标移动事件，阻止出现卡顿现象
+
 
     def mouseMoveEvent(self, *args, **kwargs):
         pass
-        # 重写鼠标移动事件，阻止出现卡顿现象
+
 
 
 # Ignore mouse wheel event on ComboBox
